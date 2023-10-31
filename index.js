c4966ccf4532c547044003aaf9e446e2da33e66b
Jhonwell Española <113007287+ryu-zaki@users.noreply.github.com>
2023-10-31 16:06:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false }));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'home.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
 
